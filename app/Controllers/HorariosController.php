43098a616ac1319de9f9b9d6f0dbab173e4cccaf
leonel15919 <makcleynm15@gmail.com>
2026-06-04 18:45:12 -0400

    holaa
    ñemaaaa

--- app/Controllers/HorariosController.php
+++ app/Controllers/HorariosController.php
@@ -1,7 +1,59 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\HorarioModel;
+
 $pageTitle = "Horarios - Sistema de Laboratorios";
 $activeRoute = "horarios";
-$viewPath = "app/Views/horarios.php";
-include "app/Views/layouts/main.php";
+
+if (isset($_GET['type'])) {
+
+    $object = new HorarioModel();
+
+    // Vista principal del cronograma semanal
+    if ($_GET['type'] == 'cronograma') {
+
+        if (isset($_POST['getCronograma'])) {
+            $result = $object->getCronogramaSemanal();
+            echo json_encode($result);
+            die();
+        }
+
+        if (isset($_POST['getCronogramaMensual'])) {
+            $result = $object->getCronogramaMensual();
+            echo json_encode($result);
+            die();
+        }
+
+        $viewPath = "app/Views/horarios/cronogramaView.php";
+        include "app/Views/layouts/main.php";
+    }
+
+    // Vista de conflictos y soluciones
+    elseif ($_GET['type'] == 'conflictos') {
+
+        if (isset($_POST['getConflictos'])) {
+            $result = $object->getConflictos();
+            echo json_encode($result);
+            die();
+        }
+
+        if (isset($_POST['resolverConflicto'])) {
+            $result = $object->resolverConflicto($_POST['idAnomalia']);
+            echo json_encode($result);
+            die();
+        }
+
+        $viewPath = "app/Views/horarios/conflictosView.php";
+        include "app/Views/layouts/main.php";
+    }
+
+    // Tipo de vista no válido
+    else {
+        echo "Error: Tipo de vista no válido.";
+    }
+
+} else {
+    $viewPath = "app/Views/horarios/cronogramaView.php";
+    include "app/Views/layouts/main.php";
+}
