834b155aa052a98286761a3817ae54764eb34249
leonel15919 <makcleynm15@gmail.com>
2026-06-04 21:48:09 -0400

    listo

--- app/Views/layouts/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'app/Views/layouts/header.php';
-include 'app/Views/layouts/sidebar.php';
-include 'app/Views/layouts/navbar.php';
-include $viewPath;
-include 'app/Views/layouts/footer.php';
